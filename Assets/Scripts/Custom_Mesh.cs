d50d50f2e042e3b42ed53dce6bbc45bfe5def5ba
PsychedelicSteve <56084753+PsychedelicSteve@users.noreply.github.com>
2024-05-25 14:18:45 -0500

    Cameras and Resolution Fix
    -Added Cameras to Church
    -Added Loader for TVs in Area 1
    -Attempted to make Dolly Cam in Church Grind Section (WIP)

---
+++ Assets/Scripts/Custom_Mesh.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Custom_Mesh : MonoBehaviour
+{
+    void Start()
+    {
+        MeshFilter meshFilter = gameObject.GetComponent<MeshFilter>();
+        MeshCollider meshCollider = gameObject.GetComponent<MeshCollider>();
+
+        // Example of creating a simple quad mesh
+        Mesh mesh = new Mesh();
+        Vector3[] vertices = new Vector3[4];
+        vertices[0] = new Vector3(0, 0, 0);
+        vertices[1] = new Vector3(1, 0, 0);
+        vertices[2] = new Vector3(0, 1, 0);
+        vertices[3] = new Vector3(1, 1, 0);
+
+        int[] triangles = new int[6];
+        triangles[0] = 0;
+        triangles[1] = 2;
+        triangles[2] = 1;
+        triangles[3] = 2;
+        triangles[4] = 3;
+        triangles[5] = 1;
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+
+        meshFilter.mesh = mesh;
+        meshCollider.sharedMesh = mesh;
+    }
+}
---
+++ Assets/Scripts/TV_Loading.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TV_Loading : MonoBehaviour
+{
+    // Array to hold the GameObjects to be loaded
+    public GameObject[] objectsToLoad;
+
+    // List to keep track of instantiated GameObjects
+    private List<GameObject> instantiatedObjects = new List<GameObject>();
+
+    // Method called when another collider enters the trigger zone
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the object entering the trigger is tagged as "Player"
+        if (other.CompareTag("Player"))
+        {
+            // Loop through each GameObject in the array
+            foreach (GameObject obj in objectsToLoad)
+            {
+                // Instantiate the GameObject at its original position and rotation
+                GameObject instantiatedObj = Instantiate(obj, obj.transform.position, obj.transform.rotation);
+                // Ensure the instantiated GameObject is active
+                instantiatedObj.SetActive(true);
+                // Add the instantiated object to the list
+                instantiatedObjects.Add(instantiatedObj);
+            }
+        }
+    }
+
+    // Method called when another collider exits the trigger zone
+    private void OnTriggerExit(Collider other)
+    {
+        // Check if the object exiting the trigger is tagged as "Player"
+        if (other.CompareTag("Player"))
+        {
+            // Loop through each instantiated GameObject in the list
+            foreach (GameObject instantiatedObj in instantiatedObjects)
+            {
+                // Destroy the instantiated GameObjects
+                Destroy(instantiatedObj);
+            }
+            // Clear the list of instantiated objects
+            instantiatedObjects.Clear();
+        }
+    }
+}
