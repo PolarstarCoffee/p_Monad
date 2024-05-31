1c3c8c8c0136c09012819097a82634c27da78717
Octavio Zuriel Zavala <zzavala2@hotmail.com>
2024-05-30 20:51:47 -0500

    Controller support for gate switch *Everything interactable is hard coded to E

--- Assets/Scripts/Supplementary_Scripts/Gate/buttonScript.cs
+++ Assets/Scripts/Supplementary_Scripts/Gate/buttonScript.cs
@@ -6,15 +6,20 @@ public class buttonScript : MonoBehaviour
 {
     // Reference to gate
     public gateScript gate;
+    private Controls inputActions;
 
     // Flag to check if the player is inside the trigger zone
     private bool isInsideTrigger = false;
-
+    private void Awake()
+    {
+        inputActions = new Controls();
+        inputActions.UI.interact.Enable();
+    }
     // Update is called once per frame
     void Update()
     {
         // Only allow the key press if the player is inside the trigger zone
-        if (isInsideTrigger && Input.GetKeyDown(KeyCode.E))
+        if (isInsideTrigger && Input.GetKeyDown(KeyCode.E) || inputActions.UI.interact.WasPressedThisFrame())
         {
             AudioManager.Instance().PlaySound("lever");
             transform.RotateAround(transform.position, new Vector3(1, 0, 0), 180);
